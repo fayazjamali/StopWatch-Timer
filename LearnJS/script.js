fc46235a3e7e74ff4bb556c292039852bd0807a7
fayazJamali <fajamalipk@gmail.com>
2023-08-26 11:35:07 +0500

    77

--- LearnJS/script.js
+++ LearnJS/script.js
@@ -28,149 +28,141 @@
 // }
 
 
-// Variables and Data Types:
 
-let age = 25;           // Declare a variable called 'age'
-const name = "John";    // Declare a constant variable called 'name'
-var score = 95;         // An older way to declare a variable (avoid using 'var')
+// // Variables and Data Types:
 
-// Data types: number, string, boolean, null, undefined, object, array
+// let age = 25;           // Declare a variable called 'age'
+// const name = "John";    // Declare a constant variable called 'name'
+// var score = 95;         // An older way to declare a variable (avoid using 'var')
 
-
-// Operators:
-
-let x = 10;
-let y = 5;
-let sum = x + y;        // Addition
-let difference = x - y; // Subtraction
-let product = x * y;    // Multiplication
-let quotient = x / y;   // Division
-let isTrue = x > y;     // Comparison (returns boolean)
+// // Data types: number, string, boolean, null, undefined, object, array
 
 
-// Conditional Statements:
+// // Operators:
 
-if (condition) {
-    // Code to execute if the condition is true
+// let x = 10;
+// let y = 5;
+// let sum = x + y;        // Addition
+// let difference = x - y; // Subtraction
+// let product = x * y;    // Multiplication
+// let quotient = x / y;   // Division
+// let isTrue = x > y;     // Comparison (returns boolean)
+
+
+// // Conditional Statements:
+
+// if (condition) {
+//     // Code to execute if the condition is true
+// } else {
+//     // Code to execute if the condition is false
+// }
+
+// // Example:
+// let num = 8;
+// if (num > 10) {
+//     console.log("Number is greater than 10");
+// } else {
+//     console.log("Number is not greater than 10");
+// }
+
+
+
+// // Loops:
+
+// // For Loop
+// for (let i = 0; i < 5; i++) {
+//     // Code to repeat
+// }
+
+// // While Loop
+// let i = 0;
+// while (i < 5) {
+//     // Code to repeat
+//     i++;
+// }
+
+
+// // Functions:
+
+// function greet(name) {
+//     return "Hello, " + name + "!";
+// }
+
+// let message = greet("Alice"); // Call the function
+
+
+
+// // Arrays:
+
+// let fruits = ["apple", "banana", "orange"];
+// console.log(fruits[0]); // Accessing array elements
+// fruits.push("grape");   // Adding an element
+
+
+// // Objects:
+
+// let person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 30
+// };
+// console.log(person.firstName); // Accessing object properties
+// person.age = 31;                // Modifying a property
+
+
+
+// // Events and Event Handling:
+
+// // HTML: <button id="myButton">Click Me</button>
+// const button = document.getElementById("myButton");
+// button.addEventListener("click", function() {
+//     // Code to run when the button is clicked
+// });
+
+
+
+// // DOM Manipulation:
+
+// // Change text of an element
+// document.getElementById("myElement").textContent = "New Text";
+
+// // Add a new element
+// let newElement = document.createElement("div");
+// newElement.textContent = "Hello!";
+// document.body.appendChild(newElement);
+
+
+// const ageInput = document.getElementById('age');
+// const submitButton = document.getElementById('submitButton');
+
+// // Add a click event listener to the submit button
+// submitButton.addEventListener('click', function() {
+//     // Get the value from the input field
+//     const ageValue = ageInput.value;
+
+//     if (ageValue < 5 ) {
+
+//         document.getElementById("text").innerHTML = "You are baby and you get it for Fre
+//     } else if (ageValue < 10 ) {
+
+//         document.getElementById("text").innerHTML = "You are Child and you get it for $5 only";
+//     } else if (ageValue < 18 ) {
+
+//         document.getElementById("text").innerHTML = "You are Teen and you get it for $10";
+//     } 
+//     else if (ageValue < 65 ) {
+
+//         document.getElementById("text").innerHTML = "You are Adult and you get it for $20";
+//     } else if (ageValue > 65 ) {
+
+//         document.getElementById("text").innerHTML = "You are Seniors! We respect you But You have to pay $22.";
+//     } 
+// });
+
+let password = prompt("Enter your new password")
+
+if (password >=) {
+    console.log("YOUR PASSWORD IS LONG ENOUGH");
 } else {
-    // Code to execute if the condition is false
+    console.log("YOUR PASSWORD MUST BE 6 OR MORE CHARACTORS LONG")
 }
-
-// Example:
-let num = 8;
-if (num > 10) {
-    console.log("Number is greater than 10");
-} else {
-    console.log("Number is not greater than 10");
-}
-
-
-
-// Loops:
-
-// For Loop
-for (let i = 0; i < 5; i++) {
-    // Code to repeat
-}
-
-// While Loop
-let i = 0;
-while (i < 5) {
-    // Code to repeat
-    i++;
-}
-
-
-// Functions:
-
-function greet(name) {
-    return "Hello, " + name + "!";
-}
-
-let message = greet("Alice"); // Call the function
-
-
-
-// Arrays:
-
-let fruits = ["apple", "banana", "orange"];
-console.log(fruits[0]); // Accessing array elements
-fruits.push("grape");   // Adding an element
-
-
-// Objects:
-
-let person = {
-    firstName: "John",
-    lastName: "Doe",
-    age: 30
-};
-console.log(person.firstName); // Accessing object properties
-person.age = 31;                // Modifying a property
-
-
-
-// Events and Event Handling:
-
-// HTML: <button id="myButton">Click Me</button>
-const button = document.getElementById("myButton");
-button.addEventListener("click", function() {
-    // Code to run when the button is clicked
-});
-
-
-
-// DOM Manipulation:
-
-// Change text of an element
-document.getElementById("myElement").textContent = "New Text";
-
-// Add a new element
-let newElement = document.createElement("div");
-newElement.textContent = "Hello!";
-document.body.appendChild(newElement);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const ageInput = document.getElementById('age');
-const submitButton = document.getElementById('submitButton');
-
-// Add a click event listener to the submit button
-submitButton.addEventListener('click', function() {
-    // Get the value from the input field
-    const ageValue = ageInput.value;
-
-    if (ageValue < 5 ) {
-
-        document.getElementById("text").innerHTML = "You are baby and you get it for Free";
-    } else if (ageValue < 10 ) {
-
-        document.getElementById("text").innerHTML = "You are Child and you get it for $5 only";
-    } else if (ageValue < 18 ) {
-
-        document.getElementById("text").innerHTML = "You are Teen and you get it for $10";
-    } 
-    else if (ageValue < 65 ) {
-
-        document.getElementById("text").innerHTML = "You are Adult and you get it for $20";
-    } else if (ageValue > 65 ) {
-
-        document.getElementById("text").innerHTML = "You are Seniors! We respect you But You have to pay $22.";
-    } 
-});
